018d6219b79c5d7f9698e10152b14d59ba25e66e
Richard Waweru <developer.waweru@gmail.com>
2018-08-04 16:58:44 +0300

    Adding date for the application to make one request if a new day is detected

--- finviz/models.py
+++ finviz/models.py
@@ -15,22 +15,30 @@ class Market(models.Model):
     def __str__(self):
         return self.company
 
+    class Meta:
+        ordering = ('id',)
+
     @classmethod
     def get_all(cls):
         portfolios = Market.objects.all()
         return portfolios
     
     @classmethod
-    def get_top_earners(cls):
-        earners = Market.objects.order_by('-change')[:10]
+    def get_top_earners(cls, date):
+        earners = Market.objects.filter(date__date=date).order_by('-change')[:10]
         return earners
     
     @classmethod
-    def get_by_country(cls, country):
-        portfolios = Market.objects.filter(country=country)
+    def get_by_country(cls, country, date):
+        portfolios = Market.objects.filter(country=country, date__date=date)
         return portfolios
 
     @classmethod
-    def get_earners_by_country(cls, country):
-        portfolios = Market.objects.filter(country=country).order_by('-change')[:10]
+    def get_earners_by_country(cls, country, date):
+        portfolios = Market.objects.filter(country=country, date__date=date).order_by('-change')[:10]
+        return portfolios
+
+    @classmethod
+    def get_today(cls, date):
+        portfolios = Market.objects.filter(date__date=date)
         return portfolios
--- finviz/views.py
+++ finviz/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .required_data import stock_data
 from .models import Market
 import json
+from threading import Timer
+from datetime import time,date
 # Drawing graphs on the chart
 from .fusioncharts import FusionCharts
 
@@ -9,36 +11,34 @@ from .fusioncharts import FusionCharts
 def home(request):
     # Get the stock data and save it in our database
     sectors, industries, countries, top_earners = [],[],[], []
-
-    # stocks = stock_data()
-    # for stock in stocks:
-    #     volume = stock['volume'].replace(',','')
-    #     change = stock['change'].replace('%','')
-    #     market = Market(ticker=stock['ticker'], company=stock['company'], sector=stock['sector'], industry=stock['industry'], country=stock['country'], price=float(stock['price']), change=float(change), volume=int(volume))
-    #     market.save()
+    get_day = date.today()
+    #Get single portfolio
+    try:
+        single_portfolio = Market.get_all().last()
+        day = single_portfolio.date.date()
+    except:
+        pass
+    
+    if day == get_day:
+        pass
+    else:
+        stocks = stock_data()
+        for stock in stocks:
+            volume = stock['volume'].replace(',','')
+            change = stock['change'].replace('%','')
+            market = Market(ticker=stock['ticker'], company=stock['company'], sector=stock['sector'], industry=stock['industry'], country=stock['country'], price=float(stock['price']), change=float(change), volume=int(volume))
+            market.save()
     
     # Feeds the navbar
-    portfolios = Market.get_all()
+    portfolios = Market.get_today(get_day)
     for portfolio in portfolios:
-        # Getting listed sectors
-        if portfolio.sector in sectors:
-            pass
-        else:
-            sectors.append(portfolio.sector)
-
-        # Getting listed industries
-        if portfolio.industry in industries:
-            pass
-        else:
-            industries.append(portfolio.industry)
-
         # Getting listed countries
         if portfolio.country in countries:
             pass
         else:
             countries.append(portfolio.country)
     
-    earners = Market.get_top_earners()
+    earners = Market.get_top_earners(get_day)
     for earner in earners:
         top_earners.append({'label':f'{earner.company}', 'value':f'{earner.change}'})
 
@@ -53,8 +53,6 @@ def home(request):
     return render(request, 'index.html', {
         'portfolios':portfolios,
         'countries':countries,
-        'industries':industries,
-        'sectors':sectors,
         'output':market_graph.render(),
         'earners':earners,
     })
@@ -62,32 +60,22 @@ def home(request):
 def country(request, country):
     # Get the stock data and save it in our database
     sectors, industries, countries, top_earners = [],[],[], []
+    get_day = date.today()
     # Feeds the navbar
     portfolios = Market.get_all()
     for portfolio in portfolios:
-        # Getting listed sectors
-        if portfolio.sector in sectors:
-            pass
-        else:
-            sectors.append(portfolio.sector)
-
-        # Getting listed industries
-        if portfolio.industry in industries:
-            pass
-        else:
-            industries.append(portfolio.industry)
-
         # Getting listed countries
         if portfolio.country in countries:
             pass
         else:
             countries.append(portfolio.country)
 
-    country_portfolios = Market.get_by_country(country)
+    country_portfolios = Market.get_by_country(country, get_day)
 
-    earners = Market.get_earners_by_country(country)
+    earners = Market.get_earners_by_country(country,get_day)
     for earner in earners:
-        top_earners.append({'label':f'{earner.company}', 'value':f'{earner.change}'})
+        if earner.change>0:
+            top_earners.append({'label':f'{earner.company}', 'value':f'{earner.change}'})
 
     # Data responsible for charting the graph
     data = {'chart': {'caption':'Top earners for today','subCaption':' ','numberSuffix':'%','theme':'ocean'},'data': top_earners}
@@ -100,9 +88,12 @@ def country(request, country):
     return render(request, 'country.html',{
         'country':country,
         'countries':countries,
-        'industries':industries,
-        'sectors':sectors,
         'portfolios':country_portfolios,
         'earners':earners,
         'output':market_graph.render(),
     })
+
+# def get_data():
+#     print('hello, world')
+
+# Timer(30, get_data).start()
